4b93ae71339208f7ceb1f92fd0d9e3816e106bd7
RICARDO LUCAS TIEPPO MARTINS <105604399+RTieppo@users.noreply.github.com>
2026-01-23 12:06:19 -0300

    ajuste teg

--- institucional/context_processors.py
+++ institucional/context_processors.py
@@ -3,6 +3,5 @@ from django.conf import settings
 
 def analytics(_request):
     return {
-        "GTM_ID": getattr(settings, "GTM_ID", ""),
         "IS_PROD": not getattr(settings, "DEBUG", True),
     }
